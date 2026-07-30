394656e188de125b3ef74dd27b4cef43c660b93a
Xyferzz <dianeshalnaura@gmail.com>
2026-07-30 18:27:25 +0700

    Bb

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
 const supabaseUrl = "https://lxqqjxddojvewtbaqodw.supabase.co";
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwanpod2drbmh4cmp1Y2RodGNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNDA5NjAsImV4cCI6MjEwMDgxNjk2MH0.Oj1zc2lPbz9AfYxH2tML_eY0CG5_pwqdVQRPwNm_sgc";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Imx4cXFqeGRkb2p2ZXd0YmFxb2R3Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUzODY5MTAsImV4cCI6MjEwMDk2MjkxMH0.P1TsnQ3HHfICHBZSSS2L7YXornEWji81yadvKBBMroQ";
 
 export const supabase = createClient(
     supabaseUrl,
