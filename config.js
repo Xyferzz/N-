512ea019cc98c9e8a063c9cc97a81fd41d57b5c6
Xyferzz <dianeshalnaura@gmail.com>
2026-07-30 12:48:51 +0700

    He

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 import { createClient } from "https://esm.sh/@supabase/supabase-js@2";
 
-const supabaseUrl = "MASUKKAN_SUPABASE_URL";
-const supabaseAnonKey = "MASUKKAN_SUPABASE_ANON_KEY";
+const supabaseUrl = "https://tpjzhwgknhxrjucdhtce.supabase.co/rest/v1/";
+const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRwanpod2drbmh4cmp1Y2RodGNlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODUyNDA5NjAsImV4cCI6MjEwMDgxNjk2MH0.Oj1zc2lPbz9AfYxH2tML_eY0CG5_pwqdVQRPwNm_sgc";
 
 export const supabase = createClient(
     supabaseUrl,
